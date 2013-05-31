3d94b412addcb61df7661ab181fa9f85a7038ee6
Mazdak Farrokhzad <twingoow@gmail.com>
2013-05-31 23:07:06 +0200

    Added documentation to WebColors.

--- aTetria-main/src/com/badlogic/gdx/graphics/WebColors.java
+++ aTetria-main/src/com/badlogic/gdx/graphics/WebColors.java
@@ -1,7 +1,30 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.badlogic.gdx.graphics;
 
 import com.badlogic.gdx.graphics.Color;
 
+/**
+ * <p>An enumeration of the WebColors according to http://en.wikipedia.org/wiki/Web_colors#X11_color_names.<br/>
+ * The colors are also partitioned into PINK_COLORS, RED_COLORS, etc.</p>
+ *
+ * @version 1.0
+ * @author BLinK3rO
+ * @author Centril<twingoow@gmail.com> / Mazdak Farrokhzad.
+ */
 public enum WebColors {
 	// Pink colors.
     PINK( new Color(1f, 0.752941176470588f, 0.796078431372549f, 1f) ),
