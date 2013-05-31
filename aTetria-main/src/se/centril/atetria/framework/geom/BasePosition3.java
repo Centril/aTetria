119334c67b7aa87c16ee69c566dff498a4cff3a0
Mazdak Farrokhzad <twingoow@gmail.com>
2013-05-31 04:46:32 +0200

    Added license (LGPL 3) where it applies.

--- aTetria-main/src/se/centril/atetria/framework/geom/BasePosition3.java
+++ aTetria-main/src/se/centril/atetria/framework/geom/BasePosition3.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of aTetria.
+ *
+ * aTetria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aTetria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aTetria. If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.centril.atetria.framework.geom;
 
 public abstract class BasePosition3 extends BasePosition implements Position3 {
